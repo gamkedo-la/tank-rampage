6f6be27a6ab88e220f23d38317c9a0e39498769c
Justin Montgomery <tryfinsigabrt@yahoo.com>
2024-03-21 23:42:40 -0500

    Add modular breakable fence and fix some physics and chaos break bugs with the fences

--- Source/TRGameplayMechanics/Private/Breakable/BreakableActorBase.cpp
+++ Source/TRGameplayMechanics/Private/Breakable/BreakableActorBase.cpp
@@ -12,8 +12,6 @@ ABreakableActorBase::ABreakableActorBase()
 	GeometryCollectionComponent = CreateDefaultSubobject<UGeometryCollectionComponent>(TEXT("Geometry Collection"));
 	SetRootComponent(GeometryCollectionComponent);
 
-	// need overlap events for hit detection (Though if respond to with OnHitComponent then this is not necessary)
-	GeometryCollectionComponent->SetGenerateOverlapEvents(true);
 	GeometryCollectionComponent->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
 }
 
