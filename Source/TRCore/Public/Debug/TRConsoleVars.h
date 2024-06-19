9d2aff6982871533e2296a6cc0301cae312cc81b
Justin Montgomery <tryfinsigabrt@yahoo.com>
2024-06-19 12:15:49 -0500

    Fix unnecessary macro redefinition

--- Source/TRCore/Public/Debug/TRConsoleVars.h
+++ Source/TRCore/Public/Debug/TRConsoleVars.h
@@ -3,10 +3,6 @@
 #include "CoreMinimal.h"
 #include "TRConstants.h"
 
-#ifndef TR_DEBUG_ENABLED
-	#define TR_DEBUG_ENABLED !(UE_BUILD_SHIPPING || UE_BUILD_TEST)
-#endif
-
 #if TR_DEBUG_ENABLED
 
 namespace TR
